6c6335cac282b695e6737a6812dc68fe0cfe9c85
MIGELZO <154762117+MIGELZO@users.noreply.github.com>
2024-10-04 22:47:29 +0300

    Add project files.

---
+++ MainWindow.xaml.cs
@@ -0,0 +1,52 @@
+﻿using System.Windows;
+using System.Windows.Input;
+
+
+namespace TestCalendar
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void LableNote_MouseDown(object sender, MouseButtonEventArgs e)
+        {
+            TxtNote.Focus();
+        }
+
+        private void TxtNote_MouseDown(object sender, System.Windows.Controls.TextChangedEventArgs e)
+        {
+            if (string.IsNullOrEmpty(LableNote.Text) && TxtNote.Text.Length > 0)
+            {
+                LableNote.Visibility = Visibility.Collapsed;
+            }
+            else
+            {
+                LableNote.Visibility = Visibility.Visible;
+
+            }
+        }
+        private void LableTime_MouseDown(object sender, MouseButtonEventArgs e)
+        {
+            TextTime.Focus();
+        }
+
+        private void TxtTime_MouseDown(object sender, System.Windows.Controls.TextChangedEventArgs e)
+        {
+            if (string.IsNullOrEmpty(TextTime.Text) && TextTime.Text.Length > 0)
+            {
+                LableTime.Visibility = Visibility.Collapsed;
+            }
+            else
+            {
+                LableTime.Visibility = Visibility.Visible;
+
+            }
+        }
+    }
+}
---
+++ UserControls/TaskMenuButton.xaml.cs
@@ -0,0 +1,34 @@
+﻿using System.Windows.Controls;
+
+
+namespace TestCalendar.UserControls
+{
+    /// <summary>
+    /// Interaction logic for TaskMenuButton.xaml
+    /// </summary>
+    public partial class TaskMenuButton : UserControl
+    {
+        public TaskMenuButton()
+        {
+            InitializeComponent();
+        }
+
+
+        //public string Caption
+        //{
+        //    get { return (string)GetValue(CaptionProperty); }
+        //    set { SetValue(Caption Property, value); }
+        //}
+
+        //public static readonly DependencyProperty Caption Property = DependencyProperty.
+        //    Register("Caption", typeof(string), typeof (TaskMenuButton));
+
+        //public FontAwesome.WPF.FontAwesomeIcon Icon
+        //        {
+        //            get { return (string)GetValue(IconProperty); }
+        //            set { SetValue(IconProperty, value); }
+        //public static readonly DependencyProperty IconProperty = DependencyProperty.
+        //Register("Icon", typeof(FontAwesome.WPF.FontAwesome Icon) typeof (MenuButton));
+        //    }
+    }
+}
